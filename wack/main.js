224e2f6e195262cf3f35366adeaafd07c6ed07c0
chokomancarr <puakai2010@gmail.com>
2023-09-17 14:42:14 +0800

    fix async bugs

--- wack/main.js
+++ wack/main.js
@@ -2,11 +2,18 @@
 'use strict';
 
 (async function() {
-document.getElementById("wack_group").innerHTML =
-    new Array(20).fill(0).map((_, i) => {
-        let x = i % 4;
-        let y = (i / 4) >> 0;
-        return `
+    let args = {};
+    (document.URL.split("?")[1] ?? "").split("&").forEach(a => {
+        let [k, v] = a.split("=");
+        args[k] = v;
+    });
+
+
+    document.getElementById("wack_group").innerHTML =
+        new Array(20).fill(0).map((_, i) => {
+            let x = i % 4;
+            let y = (i / 4) >> 0;
+            return `
 ${(y > 0) ? `
     <image id="wack_target_${x}_${y-1}" class="wack_target" data-x="${x}" data-y="${y-1}" href="../res/mochispinRTX0000.png" x="${-58 + x * 30}" y="${-88 + y * 30}" width="26" height="26"/>
     <image id="wack_target_${x}_${y-1}_bad" class="wack_target" data-x="${x}" data-y="${y-1}" href="../res/mochispinRTX0030.png" x="${-58 + x * 30}" y="${-88 + y * 30}" width="26" height="26"/>
@@ -23,15 +30,33 @@ ${(y > 0) ? `
     a 10,5 0 1,0 -20,0
     l -5, 0
     z
-" fill="white"/>
-        `}
+" fill="white"/>`;
+        }
     ).join(" ");
 
     let targets = new Array(4).fill(0).map(_ => [0,0,0,0]);
+    let canwack = new Array(4).fill(0).map(_ => Array(4).fill(false));
 
     const delay = ms => new Promise(res => setTimeout(res, ms));
     
-    async function add_target() {
+    let ovl = document.getElementById("ovl_txt");
+    
+    let playing = true;
+    let score = 0;
+
+    let t0;
+
+    const infinite = args["infinite"] === "1";
+    const MAX_TIME = 60000;
+    const end_t = infinite? -1 : t0 + MAX_TIME;
+    
+    let bar = document.getElementById("time_bar");
+    if (!infinite) bar.style.display = null;
+
+    let drop_queue = [];
+    let end_queue = [];
+
+    function add_target(t) {
         let x;
         let y;
         do {
@@ -42,16 +67,34 @@ ${(y > 0) ? `
         targets[x][y] = bad ? 2 : 1;
         let img = document.getElementById(`wack_target_${x}_${y}` + (bad ? "_bad" : ""));
         img.classList.add("show");
-        await delay(2000);
-        img.classList.remove("show");
-        await delay(1200);
-        targets[x][y] = 0;
+        drop_queue.push({
+            x: x,
+            y: y,
+            img: img,
+            t: t + 2000,
+        });
+        canwack[x][y] = true;
     }
-    
-    let ovl = document.getElementById("ovl_txt");
 
-    let playing = true;
-    let score = 0;
+    for (let tar of document.getElementsByClassName("wack_target")) {
+        tar.addEventListener("click", e => {
+            if (playing) {
+                let x = +e.target.dataset.x;
+                let y = +e.target.dataset.y;
+                if (!canwack[x][y]) return;
+                canwack[x][y] = false;
+                let f_d = drop_queue.findIndex(d => d.img === e.target);
+                if (f_d > -1) {
+                    e.target.classList.remove("show");
+                    let o = drop_queue.splice(f_d, 1)[0];
+                    o.t = t0 + 1200;
+                    end_queue.push(o);
+                }
+                score += (targets[x][y] === 1) ? 10 : -50; 
+                ovl.innerText = "SCORE: " + score;
+            }
+        });
+    }
 
     ovl.innerText = "3";
     await delay(1000);
@@ -62,43 +105,51 @@ ${(y > 0) ? `
     ovl.innerText = "GO!";
     await delay(1000);
     ovl.innerText = "SCORE: 0";
+    
+    t0= performance.now();
+    let spawn_t = t0 + 1000;
 
-    let now = Date.now();
+    function loop(t) {
+        let dt = t - t0;
+        t0 = t;
 
-    const MAX_TIME = 60;
-
-    (async function() {
-        let bar = document.getElementById("time_bar");
-        bar.style.display = null;
-        while (playing) {
-            await delay(100);
-            let t = Date.now();
-            let tn = Math.max(MAX_TIME - (t - now) * 0.001, 0);
-            bar.setAttribute("width", tn / MAX_TIME * 120 + "");
-            if (tn === 0) playing = false;
+        if (spawn_t < t) {
+            spawn_t = t + 1000;
+            add_target(t);
         }
-    })();
 
-    for (let tar of document.getElementsByClassName("wack_target")) {
-        tar.addEventListener("click", e => {
-            if (playing) {
-                let x = +e.target.dataset.x;
-                let y = +e.target.dataset.y;
-                let ty = targets[x][y];
-                if (ty === 0) return;
-                targets[x][y] = 0;
-                e.target.classList.remove("show");
-                if (ty === 1) score += 10;
-                else if (ty === 2) score -= 50;
-                ovl.innerText = "SCORE: " + score;
+        for (let a = drop_queue.length - 1; a >= 0; a -= 1) {
+            let o = drop_queue[a];
+            if (o.t < t) {
+                drop_queue.splice(a, 1);
+                end_queue.push(o);
+                o.t += 1000;
+                o.img.classList.remove("show");
             }
-        });
+        }
+
+        for (let a = end_queue.length - 1; a >= 0; a -= 1) {
+            let o = end_queue[a];
+            if (o.t < t) {
+                end_queue.splice(a, 1);
+                targets[o.x][o.y] = 0;
+                canwack[o.x][o.y] = false;
+            }
+        }
+
+        bar.setAttribute("width", (end_t - t) / MAX_TIME * 120 + "");
+
+        if (!infinite && t > end_t) {
+            bar.style.display = "none";
+            ovl.classList.add("bold");
+            playing = false;
+
+            drop_queue.forEach(d => d.img.classList.remove("show"));
+        }
+        else {
+            requestAnimationFrame(loop);
+        }
     }
 
-    while (playing) {
-        add_target();
-        await delay(1000);
-    }
-
-    ovl.classList.add("bold");
+    requestAnimationFrame(loop);
 })();
